6a960566c58160289b286cbaeb75181048c77db3
Davide Mucci <davidemucci0804@gmail.com>
2025-09-29 22:15:12 +0200

    simple UI

--- mypersonallibrary.client/src/main.js
+++ mypersonallibrary.client/src/main.js
@@ -2,5 +2,8 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-createApp(App).mount('#app')
+var app = createApp(App)
+app.use(PrimeVue).mount('#app');
